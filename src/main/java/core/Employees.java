a0813fddbf502e9b66abf5874c92673ff7f7d00d
Alexander <alexander@Alexanders-MacBook-Pro.local>
2017-03-17 21:05:23 -0700

    Initial commit

---
+++ src/main/java/core/Employees.java
@@ -0,0 +1,9 @@
+package core;
+
+public class Employees {
+	public static String employee() {
+		String s = "Perks";
+		return (s);
+	}
+}
+
---
+++ src/test/java/core/EmployeesTest.java
@@ -0,0 +1,68 @@
+package core;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class EmployeesTest {
+	@Test
+	public void f1() {
+		String s = Employees.employee();
+		assertEquals("Perks", s);
+	}
+
+	@Test
+	public void f2() {
+		String b = Employees.employee();
+		assertEquals("Perks", b);
+	}
+
+	@Test
+	public void f3() {
+		String c = Employees.employee();
+		assertEquals("Perks", c);
+	}
+
+	@Test
+	public void f4() {
+		String d = Employees.employee();
+		assertEquals("Perks", d);
+	}
+
+	@Test
+	public void f5() {
+		String e = Employees.employee();
+		assertEquals("Perks", e);
+	}
+
+	@Test
+	public void f6() {
+		String f = Employees.employee();
+		assertEquals("Perks", f);
+	}
+
+	@Test
+	public void f7() {
+		String g = Employees.employee();
+		assertEquals("Perks", g);
+	}
+
+	@Test
+	public void f8() {
+		String h = Employees.employee();
+		assertEquals("Perks", h);
+	}
+
+	@Test
+	public void f9() {
+		String i = Employees.employee();
+		assertEquals("Perks", i);
+	}
+
+	@Test
+	public void f10() {
+		String j = Employees.employee();
+		assertEquals("Perks", j);
+	}
+
+}
